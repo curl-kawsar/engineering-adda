413db879787a6c0379b0e3e2239ed633c02fdc0b
Md. Kawsar Ahmed <knownaskawsar@gmail.com>
2025-10-27 02:22:21 -0700

    resume

---
+++ src/app/resume/page.jsx
@@ -0,0 +1,5 @@
+import { redirect } from "next/navigation";
+
+export default function ResumePage() {
+  redirect("https://drive.google.com/file/d/1E1KiNnssWGo1s_zuM7bL2QjjDg02RAd1/view?usp=drive_link");
+}
